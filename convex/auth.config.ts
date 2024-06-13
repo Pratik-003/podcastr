42efc376f6efab778da3b9aea39e43745c45e0a4
Pratik-003 <maharanapratik600@gmail.com>
2024-06-13 12:41:09 +0530

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,12 @@
+// https://musical-tadpole-73.clerk.accounts.dev
+
+const authConfig= {
+    providers: [
+      {
+        domain: "https://musical-tadpole-73.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
